1a213a0d1e51744703051436f4524f5e12a7f28c
Katy <rainbow.ekaterina@gmail.com>
2011-08-18 16:24:48 +0400

    Gallery added

---
+++ gallery.php
@@ -0,0 +1,23 @@
+<?php
+?>
+<html>
+<head>
+    <meta content="text/html; charset=utf-8" http-equiv="Content-Type"/>
+    <title>Галерея изображений</title>
+    <link rel="stylesheet" type="text/css" href="gallery.css">
+</head>
+<body link="#828282" alink="#D3E2F0" vlink="#D1D1D1">
+<div id="header"><h1>Галерея изображений</h1></div>
+<div id="sidebar">
+    <p><a href="#">Агата Кристи</a></p>
+    <p><a href="#">Avril Lavigne</a></p>
+    <p><a href="#">Evanescense</a></p>
+    <p><a href="#">7B</a></p>
+    <p><a href="#">Би-2</a></p>
+</div>
+<div id="content">
+    <h2>Avril Lavigne</h2>
+</div>
+<div id="footer">&copy; Ekaterina Khurtina</div>
+</body>
+</html>
