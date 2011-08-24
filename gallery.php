6c4b580025f7289169b3a5c1de4cb7d344e6866d
Katy <rainbow.ekaterina@gmail.com>
2011-08-24 22:27:35 +0400

    Added functions to add gallery into empty folder. Some changes. Fix is procesing.

--- gallery.php
+++ gallery.php
@@ -1,4 +1,61 @@
 <?php
+
+if (isset($_GET['dir'])) {
+    $dir = $_GET['dir'];
+}
+
+if (isset($_GET['create'])) {
+    $create = $_GET['create'];
+}
+
+if (isset($_GET['gallery_dir'])) {
+    $gallery_dir=$_GET['gallery_dir'];
+    create_gallery($gallery_dir);
+}
+
+if (isset($_GET['hidden'])) {
+    $hidden = $_GET['hidden'];
+}
+function create_gallery($gallery_dir="tmpdir") {
+        $fullpath=$_GET['hidden'].$gallery_dir."/";
+        echo $fullpath;
+
+    mkdir($fullpath, 0770);
+}
+function print_tree($path = "./*")
+{
+    foreach (glob($path) as $file) {
+        if (is_dir($file)) {
+            $basename = basename($file);
+            print "<p><a href='?dir=$file/' type='dir'>$basename</a></p>";
+            print_tree($path = $file . "/*");
+        }
+    }
+}
+
+function print_images($dir = "/images/*")
+{
+    $images = glob($dir . "*.{jpg,png,jpeg,gif}", GLOB_BRACE);
+    $count_images = count($images);
+    if ($count_images == 0) {
+        $basenamedir = basename($dir);
+        print "Папка $basenamedir пуста. <a href='gallery.php?create=1&dir=$dir'> Создайте здесь свою галерею</a>!";
+    }
+
+    for ($i = 0, $k = 0; $i < ceil($count_images / 4); ++$i) {
+        print "<tr>";
+        for ($j = 0; $j < 4; ++$j, ++$k) {
+            print "<td>";
+            if ($k < $count_images) {
+                $image = rawurldecode($images[$k]);
+                print "<a href='$image'><img src='$image' height='200' width='200'></a>";
+            }
+            print "</td>";
+        }
+        print "</tr>";
+    }
+}
+
 ?>
 <html>
 <head>
@@ -7,17 +64,45 @@
     <link rel="stylesheet" type="text/css" href="gallery.css">
 </head>
 <body link="#828282" alink="#D3E2F0" vlink="#D1D1D1">
-<div id="header"><h1>Галерея изображений</h1></div>
+<div align="center" id="header"><img src="images/gallery.gif" alt="Галерея" align="middle"></div>
 <div id="sidebar">
-    <p><a href="#">Агата Кристи</a></p>
-    <p><a href="#">Avril Lavigne</a></p>
-    <p><a href="#">Evanescense</a></p>
-    <p><a href="#">7B</a></p>
-    <p><a href="#">Би-2</a></p>
+    <div class="text">
+        <?php print_tree("./*"); ?>
+    </div>
 </div>
 <div id="content">
-    <h2>Avril Lavigne</h2>
+<?php
+        if (isset($dir)) {
+    ?>
+    <table cellspacing="5">
+        <?php print_images($dir); ?>
+    </table>
+    <?php
+}
+    if (isset($gallery_dir)) { ?>
+        <div class='text'>Ваша папка с фотографиями успешно создана. Пришло время добавить немного картинок! </div><br>
+        <p>
+        <form action="gallery.php " name="createGallery" method="get" enctype="multipart/form-data">
+            <input type="file" multiple accept="image/*, image/jpeg" name="imageUploader">
+            <p>
+                <input type="image" src="images/sendbutton.png">
+                <input type="submit" value="Отправить">
+            </p>
+        </form>
+            <a href="gallery.php?dir=<?php print $hidden . $gallery_dir; ?>">Просмотреть свою папку с картинками.</a>
+    <?php }
+
+    if (isset($create) && $create == 1) { ?>
+        <form action="gallery.php" method="get" name="formChooseDir">
+            <div class="text">Введите имя папки: </div><input type="text" name="gallery_dir" value="">
+            <input type="hidden" value="<?php print $dir; ?>" name="hidden">
+            <input type="submit" value="Отправить">
+        </form>
+<!--
+    <?php }
+
+   ?>
 </div>
-<div id="footer">&copy; Ekaterina Khurtina</div>
+<div id="footer">&copy;<a href="mailto:rainxforum@gmail.com">Ekaterina Khurtina</a></div>
 </body>
 </html>
