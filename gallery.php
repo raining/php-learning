1401e5430b4ec72ab7fa1c9982c6873d6b5f4b31
Kate <rw@rw-think.(none)>
2011-08-26 22:22:29 +0400

    Added some dir verifier and error handler.

--- gallery.php
+++ gallery.php
@@ -1,40 +1,39 @@
 <?php
 
 $error_messages["empty_dir"] = "В галерее нет изображений.";
-$error_messages["not_exist_dir"] = "Запрашиваемого каталога не существует!";
+$error_messages["not_exist_dir"] = "Запрашиваемой галереи не существует!";
 $error_messages["wrong_file_extension"] = "Неподдерживаемый тип закачиваемого файла!";
 $error_messages["wrong_perm"] = "Отсутствует доступ к запрашиваемой галерее.";
 $error_messages["exist_dir"] = "Каталог уже существует!";
 
-$server_dir = $_SERVER['DOCUMENT_ROOT'];
+$server_dir = substr($_SERVER['DOCUMENT_ROOT'], 0, 13);
+//print "server_dir = $server_dir<br>";
 
+if (isset($_GET['error'])) {
+    $error = 0;
+    for ($i = 1; $i < 101; $i++) {
+        if (strnatcasecmp($_GET['error'], "$i") == 0) {
+            $error = $_GET['error'];
+            break;
+        }
+    }
+    if ($error == 0) {
+        unset($_GET['error']);
+    }
+}
 if (isset($_GET['dir'])) {
-    print realpath($_GET['dir']);
-    if (!file_exists($_GET['dir'])) {
-        //$error_messages["not_exist_dir"];
+    //    print "realpath = " . realpath($_GET['dir']) . "<br>";
+    //    print "substr = " . substr(realpath($_GET['dir']), 0, 13) . "<br>";
+
+    if (strnatcasecmp(substr(realpath($_GET['dir']), 0, 13), $server_dir) != 0) { //не равны
+        //        print $error_messages['not_exist_dir'] ." или " . $error_messages['wrong_perm'];
         $dir = "./";
-    } else {
+        Header("Location: gallery.php?error=1");
+        exit();
+    }
+    if (strnatcasecmp(substr(realpath($_GET['dir']), 0, 13), $server_dir) == 0) {
         $dir = $_GET['dir'];
     }
-}
-
-if (isset($_GET['image'])) {}
-
-if (isset($_GET['create'])) {
-    if ($_GET['create'] == 1) {
-        $create = $_GET['create'];
-    }
-}
-
-if (isset($_GET['gallery_dir'])) {
-    $gallery_dir = $_GET['gallery_dir'];
-    create_gallery($gallery_dir);
-}
-
-
-if (isset($_REQUEST['imageUploader'])) {
-    echo "$_FILES = " . print_r($_FILES, true) . "<br>";
-    echo "$_POST = " . print_r($_POST, true) . "<br>";
 }
 
 function create_gallery($gallery_dir = "tmpdir")
@@ -53,20 +52,17 @@ function print_tree($path = "./*")
     foreach (glob($path) as $file) {
         if (is_dir($file)) {
             $basename = basename($file);
-            $real = realpath($file);
-            print "<p><a href='?dir=$file/' type='dir'>$basename | realpath=$real</a></p>";
-            //            print_tree($path = $file . "/*");
+            print "<p><a href='?dir=$file/' type='dir'>$basename</a></p>";
         }
     }
 }
 
 function print_images($dir = "/images/*")
 {
-    $images = glob($dir . "*.{jpg,png,jpeg,gif}", GLOB_BRACE);
+    $images = glob(realpath($dir) . "/*.{jpg,png,jpeg,gif}", GLOB_BRACE);
     $count_images = count($images);
     if ($count_images == 0) {
-        $basenamedir = basename($dir);
-        print "В папке $basenamedir нет изображений. <a href='gallery.php?create=1&dir=$dir'> Добавить картинок в галерею</a>.";
+        ;
     }
 
     for ($i = 0, $k = 0; $i < ceil($count_images / 4); ++$i) {
@@ -74,13 +70,18 @@ function print_images($dir = "/images/*")
         for ($j = 0; $j < 4; ++$j, ++$k) {
             print "<td>";
             if ($k < $count_images) {
-                $image = rawurldecode($images[$k]);
+                $image = substr(rawurldecode($images[$k]), 14, strlen(rawurldecode($images[$k])) - 13);
                 print "<a href='$image'><img src='$image' height='200' width='200'></a>";
             }
             print "</td>";
         }
         print "</tr>";
     }
+}
+
+function generate_error($string = "error")
+{
+    print $string != "" ? "<p><div id='error'>$string</div></p>" : "";
 }
 
 ?>
@@ -91,14 +92,15 @@ function print_images($dir = "/images/*")
     <link rel="stylesheet" type="text/css" href="gallery.css">
 </head>
 <body>
-<div align="center" id="header"><a href="gallery.php"><img src="images/gallery.gif" alt="Галерея" align="middle"></a></div>
+<div align="center" id="header"><a href="gallery.php"><img src="images/gallery.gif" alt="Галерея" align="middle"></a>
+</div>
 <div id="sidebar">
     <div class="text">
 <?php
 
     if (isset($dir) && $dir != "./") {
-            $parent = dirname($dir);
-            print "<a class='home' href='gallery.php?dir=$parent/'>Назад</a><br>";
+        $parent = dirname($dir);
+        print "<a class='home' href='gallery.php?dir=$parent/'>Назад</a><br>";
         print_tree($dir . "*");
     } else {
         print_tree("./*");
@@ -108,10 +110,12 @@ function print_images($dir = "/images/*")
 </div>
 <div id="content">
 <?php
-
-    if ($_SERVER['QUERY_STRING'] == "") {
-        print_images("./*");
+    if (strnatcasecmp($error, 1) == 0) {
+        generate_error($error_messages['not_exist_dir'] . " или " . $error_messages['wrong_perm']);
+    } else {
+        generate_error($error);
     }
+    print_images("./*");
     if (isset($dir)) {
         ?>
         <table cellspacing="5">
@@ -120,36 +124,6 @@ function print_images($dir = "/images/*")
         <?php
 
     }
-    if (isset($gallery_dir)) {
-        ?>
-        <div class='text'>Ваша папка с фотографиями успешно создана. Пришло время добавить немного картинок!</div>
-        <p>
-        <form action="gallery.php" method="post" enctype="multipart/form-data">
-            <input type="file" accept="image/*, image/jpeg" name="myFile">
-            <input type="hidden" value="<?php print $gallery_dir; ?>" name="uploaddir">
-
-            <p>
-                <input type="submit" name="imageUploader" value="Отправить на сервер">
-            </p>
-        </form>
-        </p>
-        <?php
-
-    }
-
-    if (isset($create) && ($create == 1 || $create == 2)) {
-        ?>
-        <form action="gallery.php" method="get" name="formChooseDir">
-            <div class="text">Введите имя папки:</div>
-            <input type="text" name="gallery_dir" value="">
-            <input type="hidden" value="<?php print $dir; ?>" name="hidden">
-            <input type="submit" value="Отправить">
-        </form>
-
-        <?php
-
-    }
-
     ?>
 </div>
 <div id="footer">&copy; <a href="mailto:rainxforum@gmail.com">Ekaterina Khurtina</a></div>
